cad8f63b9baea98067dee43d0cdbf05653a847ea
Stephanie Messad <sabud1208@gmail.com>
2018-11-16 02:55:33 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,13 +1,14 @@
 def division(num1, num2)
- 
+ num1/num2
 end
 
 def assign_variable(value)
-  
+   name= "#{value}"
+   return name
 end
 
 def argue
-  
+
 end
 
 def greeting
@@ -25,3 +26,6 @@ end
 def pizza_party
 
 end
+
+puts division(42, 7)
+assign_variable("Bob")
